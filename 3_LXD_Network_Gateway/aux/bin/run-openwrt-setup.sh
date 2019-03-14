17b05633a087497c4f2c3c1403751c18c0803361
Kat Morgan <kat.morgan@canonical.com>
2019-03-13 22:19:35 -1000

    tweaks

--- 3_LXD_Network_Gateway/aux/bin/run-openwrt-setup.sh
+++ 3_LXD_Network_Gateway/aux/bin/run-openwrt-setup.sh
@@ -1,27 +1,30 @@
-wget -O /etc/config/dhcp      
-wget -O /etc/config/dropbear  
-wget -O /etc/config/firewall  
-wget -O /etc/config/luci      
-wget -O /etc/config/network   
-wget -O /etc/config/rpcd     
-wget -O /etc/config/squid    
-wget -O /etc/config/system    
-wget -O /etc/config/ucitrack 
-wget -O /etc/config/uhtpd    
-wget -O /etc/squid/squid.conf  
+pull_config_files () {
+wget -O /etc/config/dhcp      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/dhcp
+wget -O /etc/config/dropbear  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/dropbear
+wget -O /etc/config/firewall  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/firewall
+wget -O /etc/config/luci      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/luci
+wget -O /etc/config/network   https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/network
+wget -O /etc/config/rpcd      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/rpcd
+wget -O /etc/config/squid     https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/squid
+wget -O /etc/config/system    https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/system
+wget -O /etc/config/ucitrack  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/ucitrack
+wget -O /etc/config/uhtpd     https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/uhttpd
+wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/squid.conf 
+}
 
+run_squid_config () {
+mkdir -p /tmp/squid/cache
+mkdir -p /usr/lib/squid/log_file_daemon
 echo "/etc/squid/squid.conf" >>/etc/sysupgrade.conf
-
 /etc/init.d/squid enable
 /etc/init.d/squid stop
-mkdir -p /usr/lib/squid/log_file_daemon
-mkdir -p /tmp/squid/cache
-chmod 0777 /tmp/squid/ -R
+chmod 0777 -R /tmp/squid/
 squid -z
 /etc/init.d/squid start
 squid -k reconfigure
+}
 
-reboot
+echo "Build Complete" && reboot
 
 #################################################################################
 #TODO:    
