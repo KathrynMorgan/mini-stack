acedcdb2994233607f6de8f90b307bfc0a609a86
katamo <kat.morgan@braincraft.io>
2019-04-06 21:01:09 -1000

    tweaks

---
+++ 7_Juju_MAAS_Cloud/aux/build-cloudctl-lxd-profile.sh
@@ -0,0 +1,75 @@
+cat <<EOF >/tmp/lxd_profile_cloudctl.yaml
+config:
+  user.network-config: |
+    version: 2
+    ethernets:
+      eth0:
+        dhcp4: false
+        dhcp6: false
+        addresses: [ 10.10.0.3/24 ]
+        gateway4: 10.10.0.1
+        nameservers:
+          addresses: [ 10.10.0.10 ]
+          search: [ maas ]
+  user.user-data: |
+    #cloud-config
+    package_upgrade: true
+    packages:
+      - jq
+      - vim
+      - tree
+      - tmux
+      - byobu
+      - lnav
+      - snapd
+      - maas-cli
+      - squashfuse
+      - libvirt-bin
+      - python-pip
+    write_files:
+      - content: |
+          clouds:
+            maasctl:
+              type: maas
+              auth-types: [oauth1]
+              endpoint: http://10.10.0.10:5240/MAAS
+        path: /home/ubuntu/.juju/maasctl.yaml
+      - content: |
+          credentials:
+            maasctl:
+              admin:
+                content:
+                  auth-type: oauth1
+                  maas-oauth: ${maasctl_api_key}
+                models:
+                  default: admin
+                  controller: admin
+        path: /home/ubuntu/.juju/credentials-maasctl.yaml
+    runcmd:
+      - [apt-get, autoremove, "-y"]
+      - [pip, install, python-openstackclient]
+      - [snap, install, juju, "--classic"]
+      - [su, -l, ubuntu, /bin/bash, -c, "ssh-keygen -f ~/.ssh/id_rsa -N ''"]
+      - [su, -l, ubuntu, /bin/bash, -c, "ssh-import-id ${ssh_service}:${service_uname}"]
+      - [wget, "-P", "/usr/bin/", "https://git.io/fjLpC"]
+      - [chmod, "+x", "/usr/bin/login-maas-cli"]
+      - [virsh, net-destroy, default]
+      - [virsh, net-undefine, default]
+      - [cp, "-f", "/etc/skel/.bashrc", "/root/.bashrc"]
+      - [cp, "-f", "/etc/skel/.bashrc", "/home/ubuntu/.bashrc"]
+      - [update-alternatives, "--set", "editor", "/usr/bin/vim.basic"]
+      - [chown, "-R", "ubuntu:ubuntu", "/home/ubuntu"]
+      - [su, "-l", ubuntu, "/bin/bash", "-c", "byobu-enable"]
+description: ccio mini-stack maasctl container profile
+devices:
+  eth0:
+    name: eth0
+    nictype: macvlan
+    parent: lan
+    type: nic
+  root:
+    path: /
+    pool: default
+    type: disk
+name: cloudctl container profile
+EOF
