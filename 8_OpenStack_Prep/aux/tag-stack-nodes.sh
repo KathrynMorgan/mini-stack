ed01ab80156b33b64fcfad4622f0d9a32af4abe7
katamo <kat.morgan@braincraft.io>
2019-04-06 19:55:10 -1000

    tweaks

---
+++ 8_OpenStack_Prep/aux/tag-stack-nodes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+add_LIST=""
+tag_LIST="mini-stack compute neutron-gw osd"
+maas_PROFILE="admin"
+
+tag_nodes () {
+for stack_TAG in ${tag_LIST}; do
+	maas ${maas_PROFILE} tag update-nodes ${stack_TAG} ${add_LIST}
+done
+}
+
+append_nodes () {
+for stack_NODE in ${stack_LIST}; do
+	add_LIST="${add_LIST} add=${stack_NODE}"
+done
+}
+
+read_nodes () {
+stack_LIST=$(maas admin machines read \
+	    | jq '.[] | {hn:.hostname, system_id:.system_id}' --compact-output \
+	    | awk -F'[":,]' '/stack-../{print $11}')
+}
+
+read_nodes
+append_nodes
+tag_nodes
